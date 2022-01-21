0b1a18759476d42d42599e801c01a359a82013fc
doyou1 <56281493+doyou1@users.noreply.github.com>
2022-01-21 10:13:28 +0900

    complete TextView

--- tutorial/textviewKt/app/src/main/java/com/example/textviewkt/MainActivity.kt
+++ tutorial/textviewKt/app/src/main/java/com/example/textviewkt/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.textviewkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {    // 앱이 최초 실행됐을 때 실행
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)  // xml 화면 뷰 연결
+        var tv_title : TextView = findViewById(R.id.tv_title)
+        tv_title.text = "Hello World!!!!!"  // 텍스트뷰의 값 변경
     }
 }
