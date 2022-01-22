88cb010acdcc94f0e55b0cb9258dd7ae53fcdba1
doyou1 <56281493+doyou1@users.noreply.github.com>
2022-01-22 17:18:43 +0900

    complete ActivityResultAPI-ImagePicker(replacement 'deprecated StartActivityForResult') & add result screenshot

--- see-also/ActivityResultAPI-ImagePicker/app/src/main/java/com/example/activityresultapi_imagepicker/MainActivity.kt
+++ see-also/ActivityResultAPI-ImagePicker/app/src/main/java/com/example/activityresultapi_imagepicker/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.activityresultapi_imagepicker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,23 +17,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btn.setOnClickListener {
-            val intent = Intent()
-            intent.type = "image/*"
-            intent.action = Intent.ACTION_GET_CONTENT
-            startActivityForResult(Intent.createChooser(intent, "Select the Image"), 100)
-        }
-
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == 100) {
-
-            if (data != null) {
-                binding.ivPicture.setImageURI(data.data)
+        val getImage = registerForActivityResult(
+            ActivityResultContracts.GetContent(),
+            ActivityResultCallback {
+                binding.ivPicture.setImageURI(it)
             }
+        )
+
+        binding.btn.setOnClickListener {
+            getImage.launch("image/*")
         }
+
     }
 }
